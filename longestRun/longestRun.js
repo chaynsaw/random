eb43f770b4684a0c023c24455e2c465976c39db0
Chaynor Hsiao <chaynsaw@gmail.com>
2019-06-28 19:27:42 -0800

    Initial commit

---
+++ longestRun/longestRun.js
@@ -0,0 +1,40 @@
+/**
+ * Write a function that, given a string, Finds the longest run of identical
+ * characters and returns an array containing the start and end indices of
+ * that run. If there are two runs of equal length, return the first one.
+ * For example:
+ *
+ *   longestRun("abbbcc") // [1, 3]
+ *   longestRun("aabbc")  // [0, 1]
+ *   longestRun("abcd")   // [0, 0]
+ *   longestRun("")       // null
+ *
+ * Try your function with long, random strings to make sure it handles large
+ * inputs well.
+ */
+
+var longestRun = function (string) {
+  let longestString = [0, 0]
+  if (Math.max(...longestString) === 0) {
+    if (string[i] === string[i+1]) {
+      longestString[0] = i;
+      longestString[1] = i + 1;
+    }
+  } else if (string[i] === string[longestString[0]]) {
+    longestString[1] = i; 
+  }
+  
+};
+
+// If you need a random string generator, use this!
+// (you wont need this function for your solution but it may help with testing)
+var randomString = function (len) {
+  var text = '';
+  var possible = 'abcdefghijklmnopqrstuvwxyz';
+
+  for (var i = 0; i < len; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
